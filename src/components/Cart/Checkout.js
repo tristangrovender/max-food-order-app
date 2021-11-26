f1a0fc0c4284c6ed9a1c392ffa27962cfb60fc98
Tristan Grovender <46428421+tristangrovender@users.noreply.github.com>
2021-11-25 19:00:55 -0600

    Adding better user feedback

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -29,7 +29,7 @@ const Checkout = props => {
     const enteredNameIsValid = !isEmpty(enteredName);
     const enteredStreetIsValid = !isEmpty(enteredStreet);
     const enteredCityIsValid = !isEmpty(enteredCity);
-    const enteredPostalCodeIsValid = !isFiveChars(enteredPostalCode);
+    const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
 
     setFormInputsValidity({
       name: enteredNameIsValid,
@@ -47,44 +47,48 @@ const Checkout = props => {
     if (!formIsValid) {
       return;
     }
+
+    props.onConfirm({
+      name: enteredName,
+      street: enteredStreet,
+      city: enteredCity,
+      postalCode: enteredPostalCode
+    });
   };
+
+  const nameControlClasses = `${classes.control} ${
+    formInputsValidity.name ? "" : classes.invalid
+  }`;
+  const streetControlClasses = `${classes.control} ${
+    formInputsValidity.street ? "" : classes.invalid
+  }`;
+  const postalCodeControlClasses = `${classes.control} ${
+    formInputsValidity.postalCode ? "" : classes.invalid
+  }`;
+  const cityControlClasses = `${classes.control} ${
+    formInputsValidity.city ? "" : classes.invalid
+  }`;
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div
-        className={`${classes.control} ${
-          formInputsValidity.name ? "" : classes.invalid
-        }`}
-      >
+      <div className={nameControlClasses}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" ref={nameInputRef} />
         {!formInputsValidity.name && <p>Please enter a valid name!</p>}
       </div>
-      <div
-        className={`${classes.control} ${
-          formInputsValidity.street ? "" : classes.invalid
-        }`}
-      >
+      <div className={streetControlClasses}>
         <label htmlFor="street">Street</label>
         <input type="text" id="street" ref={streetInputRef} />
         {!formInputsValidity.street && <p>Please enter a valid street!</p>}
       </div>
-      <div
-        className={`${classes.control} ${
-          formInputsValidity.postalCode ? "" : classes.invalid
-        }`}
-      >
+      <div className={postalCodeControlClasses}>
         <label htmlFor="postal">Postal Code</label>
         <input type="text" id="postal" ref={postalCodeInputRef} />
         {!formInputsValidity.postalCode && (
-          <p>Please enter a valid postal code!</p>
+          <p>Please enter a valid postal code (5 characters long)!</p>
         )}
       </div>
-      <div
-        className={`${classes.control} ${
-          formInputsValidity.city ? "" : classes.invalid
-        }`}
-      >
+      <div className={cityControlClasses}>
         <label htmlFor="city">City</label>
         <input type="text" id="city" ref={cityInputRef} />
         {!formInputsValidity.city && <p>Please enter a valid city!</p>}
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -14,9 +14,7 @@ const cartReducer = (state, action) => {
     const existingCartItemIndex = state.items.findIndex(
       item => item.id === action.item.id
     );
-
     const existingCartItem = state.items[existingCartItemIndex];
-
     let updatedItems;
 
     if (existingCartItem) {
@@ -35,7 +33,6 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount
     };
   }
-
   if (action.type === "REMOVE") {
     const existingCartItemIndex = state.items.findIndex(
       item => item.id === action.id
@@ -57,6 +54,10 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -74,11 +75,16 @@ const CartProvider = props => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
-    removeItem: removeItemFromCartHandler
+    removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler
   };
 
   return (
